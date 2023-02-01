61ba9c04df05d8b7b2ad751defbeb562cc5f4c99
saskiabntg16 <123155307+saskiabntg16@users.noreply.github.com>
2023-02-01 22:20:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 from sklearn import datasets
-from sklearn.svm import GaussianNB
+from sklearn.svm import SVC
 import pickle 
 
 st.write("""
