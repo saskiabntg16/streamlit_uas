b5fedb7690edae7573fe93280015c625ac11c3dc
saskiabntg16 <123155307+saskiabntg16@users.noreply.github.com>
2023-02-01 23:26:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,15 +15,15 @@ img = Image.open ('iris.png')
 st.image(img, use_column_width=False)
 
 def input_user():
-SepalLengthCm=st.slider('SepalLength:', 2.0, 6.0)
-SepalWidthCm=st.slider('SepalWidthCm:', 0.0, 5.0)
-PetalLengthCm=st.slider('PetalLengthCm', 0.0, 3.0)
-PetalWidthCm=st.slider('PetalWidthCm:', 0.0, 2.0)
-
-data = {'SepalLength':SepalLengthCm,
-        'SepalWidth':SepalWidthCm,
-        'PetalLength':PetalLengthCm,
-        'PetalWidth':PetalWidthCm}
+        
+SepalLengthCm = st.slider('SepalLengthCm:', 2.0, 6.0)
+SepalWidthCm = st.slider('SepalWidthCm:', 0.0, 5.0)
+PetalLengthCm = st.slider('PetalLengthCm',0.0, 3.0)
+PetalWidthCm = st.slider('PetalWidthCm:', 0.0, 2.0)
+data = {'SepalLengthCm': SepalLengthCm,
+        'SepalWidthCm': SepalWidthCm,
+        'PetalLengthCm': PetalLengthCm,
+        'PetalWidthCm': PetalWidthCm}
      
 fitur = pd.DataFrame(data, index=[0]) 
 return fitur
