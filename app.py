23ca3a126d598971d13415a32fc1e0e2affd3fa3
saskiabntg16 <123155307+saskiabntg16@users.noreply.github.com>
2023-02-01 22:29:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,16 +17,15 @@ st.image(img, use_column_width=False)
 
 st.sidebar.header('Parameter Value')
 
-def input_user():
-    SepalWidthCm = st.slider('SepalWidthCm:', 2.0, 6.0)
-    SepalLengthCm= st.slider('SepalLengthCm:', 0.0, 5.0)
-    PetalWidthCm = st.slider('PetalWidthCm',0.0, 3.0)
-    PetalLengthCm = st.slider('PetalLengthCm:', 0.0, 2.0)
+SepalLengthCm = st.slider('SepalLengthCm:', 2.0, 6.0)
+SepalWidthCm = st.slider('SepalWidthCm:', 0.0, 5.0)
+PetalLengthCm = st.slider('PetalLengthCm',0.0, 3.0)
+PetalWidthCm = st.slider('PetalWidthCm:', 0.0, 2.0)
 
-data = {'SepalWidthCm': SepalWidthCm,
-            'SepalLengthCm': SepalLengthCm,
-            'PetalWidthCm': PetalWidthCm,
-            'PetalLengthCm': PetalLengthCm}
+data = {'SepalLengthCm': SepalLengthCm,
+        'SepalWidthCm': SepalWidthCm,
+        'PetalLengthCm': PetalLengthCm,
+        'PetalWidthCm': PetalWidthCm}
      
 features = pd.DataFrame(data, index=[0]) 
 
