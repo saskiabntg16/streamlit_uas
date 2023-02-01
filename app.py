9f8ae65a4728437b1e0b0dd3cda768190f442e4c
saskiabntg16 <123155307+saskiabntg16@users.noreply.github.com>
2023-02-02 00:23:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,18 +15,16 @@ img = Image.open ('iris.png')
 st.image(img, use_column_width=False)
 st.write("Please Insert Values, to Get Iris Class prediction:")
 
-def input():
-SepalLengthCm = st.slider('SepalLengthCm:', 2.0, 6.0)
-SepalWidthCm = st.slider('SepalWidthCm:', 0.0, 5.0)
-PetalLengthCm = st.slider('PetalLengthCm',0.0, 3.0)
-PetalWidthCm = st.slider('PetalWidthCm:', 0.0, 2.0)
+SepalLengthCm = st.sidebar.slider('SepalLengthCm:', 2.0, 6.0)
+SepalWidthCm = st.sidebar.slider('SepalWidthCm:', 0.0, 5.0)
+PetalLengthCm = st.sidebar.slider('PetalLengthCm',0.0, 3.0)
+PetalWidthCm = st.sidebar.slider('PetalWidthCm:', 0.0, 2.0)
 data = {'SepalLengthCm': SepalLengthCm,
         'SepalWidthCm': SepalWidthCm,
         'PetalLengthCm': PetalLengthCm,
         'PetalWidthCm': PetalWidthCm}
 
 features = pd.DataFrame(data, index=[0])
-df = input()
 
 st.subheader('Parameter Inputan')
 st.write(df)
